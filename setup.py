7f4ceb50d174c71d8a1a711f5d1d6ae0286fbc0c
cyril6 <cyril@abtasty.com>
2021-09-06 11:56:00 +0200

    fix long description content  type

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     name="mockseries",
     version="0.1.1",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     url="http://mockseries.catheu.tech/",
     license="BSD",
