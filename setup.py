3e2563731b6082ca93b0f0cc80e40fae901ad05f
cyril6 <cyril@abtasty.com>
2021-09-15 16:18:44 +0200

    fix incorrect default behaviour for stop window

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mockseries",
-    version="0.1.2",
+    version="0.1.3",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
