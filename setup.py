e8feccd01dae9be9c410d28a937ece13d32eecee
Cyril de Catheu <cyril@startree.ai>
2023-10-28 20:00:51 +0200

    bump to next version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mockseries",
-    version="0.2.0",
+    version="0.2.1",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
