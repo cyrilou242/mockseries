ae3ca88d15316ee9d0df4e0c6f708b0bb3ba463e
Cyril de Catheu <cdecatheu@hey.com>
2024-05-23 16:34:14 +0200

    bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="mockseries",
-    version="0.3.0",
+    version="0.3.1",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
